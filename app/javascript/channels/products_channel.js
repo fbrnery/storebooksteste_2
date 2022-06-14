fa9944fa02e6206d7ceeaae8fe0ab2427729bac9
Fabricio Miranda <fbrnery@gmail.com>
2022-06-14 10:35:32 -0300

    cap:12 - pag 179 - Task G: Check Out

--- app/javascript/channels/products_channel.js
+++ app/javascript/channels/products_channel.js
@@ -5,7 +5,7 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-***/
+***////////////
 import consumer from "./consumer"
 
 consumer.subscriptions.create("ProductsChannel", {
