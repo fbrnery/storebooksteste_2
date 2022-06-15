9ee099a083f9c510928ab7beeb1e9ba6784df3d7
Fabricio Miranda <fbrnery@gmail.com>
2022-06-15 18:55:53 -0300

    cap 13 - página 198 - Task H: Entering Additional

---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @order = orders(:one)
+  end
+
+  test "should show order" do
+    get order_url(@order)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_order_url(@order)
+    assert_response :success
+  end
+
+  test "should update order" do
+    patch order_url(@order), params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+    assert_redirected_to order_url(@order)
+  end
+
+  test "should destroy order" do
+    assert_difference('Order.count', -1) do
+      delete order_url(@order)
+    end
+
+    assert_redirected_to orders_url
+  end
+end
