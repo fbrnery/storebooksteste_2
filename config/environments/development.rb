e32b7a19d936d332552006cb40e4967040d1b964
Fabricio Miranda <fbrnery@gmail.com>
2022-06-23 15:18:17 -0300

    cap 14 página 230 - Correction smtp-continuação pag 241-cap15

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
