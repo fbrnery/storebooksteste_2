1bc5010d0314fe7b44e2f7f0240c53eaffdd0380
Fabricio Miranda <fbrnery@gmail.com>
2022-06-28 00:28:16 -0300

    cap 17 pagina 288-Task L: Receive Emails and Respond

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,9 +35,9 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice: 
-          'Thank you for your order.' }
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
+        format.html { redirect_to store_index_url(locale: I18n.locale), 
+          notice: I18n.t('.thanks') }
         format.json { render :show, status: :created,
           location: @order }
       else
